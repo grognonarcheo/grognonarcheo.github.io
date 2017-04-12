6f459a76a15483247ad972972bb4178af0b840d2
grognonarcheo <elise.delaunois@gmail.com>
2017-04-12 20:30:25 +0200

    Hotspots
    Added captions for hotspots

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-group-0-img_0826_img_0886-61-images",
-      "name": "[Group 0]-IMG_0826_IMG_0886-61 images",
+      "id": "0-le-grognon---avril-2017",
+      "name": "Le Grognon - avril 2017",
       "levels": [
         {
           "tileSize": 256,
@@ -33,14 +33,33 @@ var APP_DATA = {
         "fov": 1.5707963267948966
       },
       "linkHotspots": [],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": 1.240416715173886,
+          "pitch": 0.03891116568887121,
+          "title": "Quartier du Grognon",
+          "text": "Des maisons détruites entre 1959 et 1973, seules les caves subsistent."
+        },
+        {
+          "yaw": -2.408166194947503,
+          "pitch": 0.18709717519411306,
+          "title": "Rue Saint-Hilaire",
+          "text": "Cette rue reliait la Place Saint-Hilaire et la pointe du Grognon"
+        },
+        {
+          "yaw": 0.0980706367120785,
+          "pitch": -0.34919512311004297,
+          "title": "Parlement Wallon",
+          "text": "Le parlement wallon est installé dans l'ancien Hospice Saint-Gilles."
+        }
+      ]
     }
   ],
-  "name": "Project Title",
+  "name": "Les fouilles du Grognon",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
